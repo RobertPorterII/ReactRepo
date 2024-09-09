52ab7236ac7eae505ee3ba0f23a8018ef04e11cb
RobertPorterII <au8494@protonmail.com>
2024-09-09 15:50:22 -0400

    React Repo

---
+++ alab320h/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+function Footer() {
+    return(
+        <footer>
+        <nav aria-label="Main Navigation" role="navigation">
+          <ul>
+            <li><a href="...">Women's</a></li>
+            <li><a href="...">Men's</a></li>
+            <li><a href="...">On The Street</a></li>
+  
+            <li><a href="..."> The Catwalk</a></li>
+            <li><a href="..."> AdWatch</a></li>
+            <li><a href="..."> About</a></li>
+            <li><a href="..."> Tips</a></li>
+          </ul>
+        </nav>
+  
+        &copy; 2013 Valet Industries,Inc
+      </footer>
+    );
+    
+}
+
+export default Footer;
---
+++ alab320h/src/components/Header.jsx
@@ -0,0 +1,28 @@
+
+
+
+function Header() {
+    return(
+        <header>
+      
+      <h1>Sartre's List</h1>
+      <h3>Better-Dressed People</h3>
+      <nav aria-label="Main Navigation" role="navigation">
+        <ul>
+          <li><a href="...">Women's</a></li>
+          <li><a href="...">Men's</a></li>
+          <li><a href="...">On The Street</a></li>
+
+          <li><a href="..."> The Catwalk</a></li>
+          <li><a href="..."> The AdWatch</a></li>
+          <li><a href="...">About</a></li>
+        </ul>
+      </nav>
+    </header>
+       
+
+    )
+    
+};
+// capital on file and component name
+export default Header;
